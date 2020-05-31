d14116734865635788c6e871ce8a677cf60b2de9
João Lourenço <estudante.lourenco@gmail.com>
2020-05-31 17:35:04 +0100

    Add users app and model

--- backend/src/radiologo/settings.py
+++ backend/src/radiologo/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'programs',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -82,6 +84,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
