05a34614275c5681d785f68675340c05cf9acbeb
João Lourenço <estudante.lourenco@gmail.com>
2020-06-01 11:23:37 +0100

    Reformat, phone number changes, and translations
    Made the file adhere to PEP standards regarding spacing.
    
    Phone number is now allowed to be bigger to support foreign phone #s.
    This is sort of dirty, because phone numbers should have their country
    code associated to perform some sort of validation... But this is way
    too low in priority, as this field is merely informative and is rarely
    used IRL.
    
    Marked more strings as needing to be translated.

--- backend/src/users/models/user.py
+++ backend/src/users/models/user.py
@@ -7,16 +7,17 @@ from django.utils import timezone
 from ..managers import CustomUserManager
 
 document_choices = (
-    ('CC', 'Cartão de Cidadão'),
-    ('PP', 'Passaporte'),
-    ('CCo', 'Carta de Condução'),
-    ('TR', 'Título de Residência'))
+    ('CC', _('Citizen Card')),
+    ('PP', _('Passport')),
+    ('CCo', _('Driving License')),
+    ('TR', _('Residence Title')))
 
 student_type_choices = (
-    ('Y', 'Sim'),
-    ('N', 'Ex-Aluno'),
-    ('E', 'Externo'))
+    ('Y', _('Yes')),
+    ('N', _('Ex-Student')),
+    ('E', _('External')))
 
+# The following are not translated, because they are titles particular to the radio
 state_choices = (
     ('MM', 'Membro'),
     ('CO', 'Colaborador'),
@@ -38,27 +39,28 @@ role_choices = (
     ('EL', 'Elemento'),
     ('NA', 'Não Atribuído'))
 
+
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(_('email address'), unique=True)
+    email = models.EmailField(_('Email address'), unique=True)
     is_staff = models.BooleanField(default=False)
 
     # Profile fields
-    author_name    = models.CharField(verbose_name =_("Author name"), max_length=75, unique=True)
-    full_name      = models.CharField(verbose_name =_("Full name"), max_length=100, unique=True)
-    id_type        = models.CharField(verbose_name = _("ID Type"), max_length=3, choices = document_choices)
-    id_number      = models.CharField(verbose_name = _("ID Number"), max_length=30, unique=True)
-    ist_student_options = models.CharField(verbose_name = _("IST Student"), max_length=1, choices = student_type_choices)
-    ist_student_number  = models.CharField(verbose_name = _("IST Student Number"), max_length=15, unique=True, null=True, blank=True)
-    phone          = models.CharField(verbose_name = _("Phone"), max_length=9, unique=True)
-    state          = models.CharField(verbose_name = _("State"), max_length=2, choices = state_choices, default="MM")
-    entrance_date  = models.DateField(verbose_name = _("Entrance Date"), default=timezone.now)
-    department     = models.CharField(verbose_name = _("Department"), max_length=2, choices = department_choices, default="NA")
-    role           = models.CharField(verbose_name = _("Role"), max_length=2, choices = role_choices, default="NA")
-    notes          = models.TextField(verbose_name = _("Notes"), default=None, null=True, blank=True)
-    date_joined    = models.DateTimeField(default=timezone.now)
-    exit_date      = models.DateField(verbose_name = _("Exit Date"), default=None, null=True, blank=True)
+    author_name = models.CharField(verbose_name=_("Author name"), max_length=75, unique=True)
+    full_name = models.CharField(verbose_name=_("Full name"), max_length=100, unique=True)
+    id_type = models.CharField(verbose_name=_("ID Type"), max_length=3, choices=document_choices)
+    id_number = models.CharField(verbose_name=_("ID Number"), max_length=30, unique=True)
+    ist_student_options = models.CharField(verbose_name=_("IST Student"), max_length=1, choices=student_type_choices)
+    ist_student_number = models.CharField(verbose_name=_("IST Student Number"), max_length=15, unique=True, null=True,
+                                          blank=True)
+    phone = models.CharField(verbose_name=_("Phone"), max_length=15, unique=True)
+    state = models.CharField(verbose_name=_("State"), max_length=2, choices=state_choices, default="MM")
+    entrance_date = models.DateField(verbose_name=_("Entrance Date"), default=timezone.now)
+    department = models.CharField(verbose_name=_("Department"), max_length=2, choices=department_choices, default="NA")
+    role = models.CharField(verbose_name=_("Role"), max_length=2, choices=role_choices, default="NA")
+    notes = models.TextField(verbose_name=_("Notes"), default=None, null=True, blank=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    exit_date = models.DateField(verbose_name=_("Exit Date"), default=None, null=True, blank=True)
     is_active = models.BooleanField(default=True)
-
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
