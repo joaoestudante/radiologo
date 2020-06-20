33ae2bc59a5d14262b408afe01a204b70c0e96b2
João Lourenço <estudante.lourenco@gmail.com>
2020-06-20 11:56:09 +0100

    Add users endpoints, refactor user service
    Endpoints are for creation, listing, updating, deleting... The usual.
    
    User service was refactored to be the only place where users are
    created. The serializer will probably need later refactoring to access
    the service for updating instead of updating it itself.

--- backend/src/users/tests/test_users.py
+++ backend/src/users/tests/test_users.py
@@ -1,12 +1,11 @@
 from django.test import TestCase
-from django.contrib.auth import get_user_model
+from ..services.UserService import UserService
 
 
 class UsersManagersTests(TestCase):
 
     def test_create_user(self):
-        User = get_user_model()
-        user = User.objects.create_user(email='normal@user.com', password='foo')
+        user = UserService().create_user(email='normal@user.com', password='foo')
         self.assertEqual(user.email, 'normal@user.com')
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
@@ -18,15 +17,14 @@ class UsersManagersTests(TestCase):
         except AttributeError:
             pass
         with self.assertRaises(TypeError):
-            User.objects.create_user()
+            UserService().create_user()
         with self.assertRaises(TypeError):
-            User.objects.create_user(email='')
+            UserService().create_user(email='')
         with self.assertRaises(ValueError):
-            User.objects.create_user(email='', password="foo")
+            UserService().create_user(email='', password='foo')
 
     def test_create_superuser(self):
-        User = get_user_model()
-        admin_user = User.objects.create_superuser('super@user.com', 'foo')
+        admin_user = UserService().create_superuser(email='super@user.com', password='foo')
         self.assertEqual(admin_user.email, 'super@user.com')
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
@@ -37,6 +35,3 @@ class UsersManagersTests(TestCase):
             self.assertIsNone(admin_user.username)
         except AttributeError:
             pass
-        with self.assertRaises(ValueError):
-            User.objects.create_superuser(
-                email='super@user.com', password='foo', is_superuser=False)
