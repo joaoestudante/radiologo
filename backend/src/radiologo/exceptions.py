c6d4b2b3342f90ac5c6212bd1f44a161082facec
João Lourenço <estudante.lourenco@gmail.com>
2020-06-21 15:09:16 +0100

    Add invitations for users
    When a user is registered, there shouldn't be a password set. He/she
    should be invited to manually set his/her password on the frontend. For
    this, an invite is created when the user is created through the service,
    which contains a token and other info required to make sure the user
    matches. After some validation, the chosen password is set.
    
    The invite is sent through the email associated to the user, and is sent
    as an HTML email or, if the email client does not support it, as plain
    text.
    
    The invite is only sent if the user is active: we want to be able to
    register old users which may not be participating in the Radio anymore,
    and so it doesn't make sense to send them an email to set a password.

--- backend/src/radiologo/exceptions.py
+++
@@ -1,2 +0,0 @@
-class RadiologoException(Exception):
-    pass
---
+++ backend/src/users/models/invite.py
@@ -0,0 +1,44 @@
+import os
+import struct
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core import signing
+from django.core.mail import EmailMultiAlternatives
+from django.db import models
+from django.utils import timezone, crypto
+from .user import CustomUser
+
+
+
+class Invite(models.Model):
+    invited_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    sent_token = models.CharField(max_length=256, default="")
+    accepted = models.BooleanField(default=False)
+    last_date_sent = models.DateTimeField(default=timezone.now)
+
+    def invited_user_name(self):
+        return self.invited_user.full_name
+
+    def generate_token(self):
+        # Inspired by: https://github.com/aaugustin/django-sesame/
+
+        raw_token = struct.pack(str("!i"), self.invited_user.pk) + crypto.pbkdf2(self.invited_user.email, "radiologo",
+                                                                                 10000)
+        url_ready_token = signing.TimestampSigner(salt="radiologo").sign(signing.b64_encode(raw_token).decode())
+        self.sent_token = url_ready_token
+        return url_ready_token
+
+    def send(self):
+        token = self.generate_token()
+        subject, from_email, to = "Bem vindo à Rádio Zero", settings.EMAIL_HOST_USER, self.invited_user.email
+        with open(settings.EMAIL_TXT, 'r') as txt:
+            text_content = txt.read().replace("{{ REGISTER_LINK }}", settings.BASE_URL + "register/" + token)
+        with open(settings.EMAIL_HTML, 'r') as html:
+            html_content = html.read().replace("{{ REGISTER_LINK }}", settings.BASE_URL + "register/" + token)
+        msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+        self.sent_token = token
+        self.last_date_sent = timezone.now()
+        self.save()
