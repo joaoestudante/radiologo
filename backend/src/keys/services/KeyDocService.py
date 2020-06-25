cd69de764146491f055dcc036cb804bdac13f731
João Lourenço <estudante.lourenco@gmail.com>
2020-06-25 11:17:01 +0100

    Add hashing to pdf generation, refactor service, info view
    For security reasons, we want to make sure we keep a trail of all pdfs
    generated, even if we discard previously generated PDFs as we generate
    new ones. They should always be accessible on the email, but by hashing
    the PDF, we can ensure that the file on the email is, indeed, the file
    produced by the server.
    
    The KeyDocService was refactored to make the main method shorter, and
    delegate tasks to other helper methods.
    
    The new view makes it easy to find out a few stats about the latest
    doc generated, for future frontend validation and display.

--- backend/src/keys/services/KeyDocService.py
+++ backend/src/keys/services/KeyDocService.py
@@ -1,9 +1,13 @@
 import locale
 import os
+import hashlib
 from datetime import datetime
 import subprocess
+
+from django.db import transaction
 from django.template.loader import render_to_string
 
+from exceptions.radiologoexception import NoNewMembersForKeyDocException
 from ..models.keydoc import KeyDoc
 from django.contrib.auth import get_user_model
 from django.template.loaders.filesystem import Loader
@@ -13,19 +17,37 @@ from unidecode import unidecode
 
 
 class KeyDocService:
-    def __init__(self, model):
-        self.keydoc = model
+    def __init__(self, user):
+        self.keydoc = None
+        self.user = user
 
     def decode_ascii(self, value):
         return unidecode(value)
 
+    @transaction.atomic
     def generate_file(self):
-        template_location = settings.KEYS_TEMPLATE_DIR
-        image_location = template_location + "rz_banner.png"
+        try:
+            previous_doc = KeyDoc.objects.latest('pk')
+        except KeyDoc.DoesNotExist:
+            previous_doc = None
+        self.keydoc = KeyDoc(creator=self.user, members_when_created=get_user_model().objects.count())
 
+        self.checkNeedsGeneration(previous_doc, self.keydoc)
+        context = self.build_context()
+        final_md = render_to_string(settings.KEYDOC_MD, context)
+        pdf_location = self.build_pdf(final_md)
+
+        with open(pdf_location, "rb") as pdf:
+            pdf_hash = hashlib.sha256(pdf.read()).hexdigest()
+        self.keydoc.doc_hash = pdf_hash
+        self.keydoc.save()
+
+        return pdf_location
+
+    def build_context(self) -> dict:
+        image_location = settings.KEYS_TEMPLATE_DIR + "rz_banner.png"
         locale.setlocale(locale.LC_TIME, "pt_PT.utf8")
         today = datetime.now().strftime("%A, %d %B %Y").title()
-
         signature = "{} ({}, {})".format(self.keydoc.creator.author_name, self.keydoc.creator.phone,
                                          self.keydoc.creator.email)
         signature_space = "_" * len(signature)
@@ -46,28 +68,33 @@ class KeyDocService:
                 id_number = "{} ({})".format(user.id_number, user.get_id_type_display())
 
             members_list.append("|{}|{}|{}|\n".format(user.author_name, id_number, ist_number))
-
         members_list_str = "".join(members_list)
-        context = {
+
+        return {
             "image": image_location,
             "date": today,
             "signature_space": signature_space,
             "signature": signature,
             "list": members_list_str,
         }
-        final_md = render_to_string(settings.KEYDOC_MD, context)
 
+    def build_pdf(self, final_md):
         output_dir = os.getcwd() + "/keys/generated/"
         if not os.path.exists(output_dir):
             os.mkdir(output_dir)
         with open(output_dir + "latest_keydoc.md", "w") as md:
             md.write(final_md)
-
         subprocess.run(
             ["pandoc", output_dir + "latest_keydoc.md", "-o", output_dir + "latest_keydoc.pdf", "--pdf-engine=xelatex",
              "--template",
-             template_location + "eisvogel.tex"])
+             settings.KEYS_TEMPLATE_DIR + "eisvogel.tex"])
+        return output_dir + "latest_keydoc.pdf"
 
-        self.keydoc.doc = output_dir + "latest_keydoc.pdf"
-        self.keydoc.save()
-        return self.keydoc.doc
+    def checkNeedsGeneration(self, previous_doc, new_doc):
+        if previous_doc is None:
+            # Never generated a PDF, safe to go
+            return
+        if previous_doc.members_when_created == new_doc.members_when_created:
+            raise NoNewMembersForKeyDocException
+        else:
+            return
--- backend/src/keys/views/KeyDocView.py
+++ backend/src/keys/views/KeyDocView.py
@@ -1,33 +1,31 @@
+import json
 from datetime import datetime
 from wsgiref.util import FileWrapper
 
+import pytz
 from django.conf import settings
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from ..models.keydoc import KeyDoc
+from ..serializers.KeyDocSerializer import KeyDocSerializer
 from ..services.KeyDocService import KeyDocService
 from django.core.mail import EmailMultiAlternatives
-
+from django.utils import timezone
 
 class KeyDocGenerate(APIView):
-    def get(self, request):
+    def post(self, request):
         now = datetime.now()
-        try:
-            doc = KeyDoc.objects.get(pk=1)
-            doc.creator = request.user
-        except KeyDoc.DoesNotExist:
-            doc = KeyDoc(creator=request.user)
-
-        service = KeyDocService(doc)
+        service = KeyDocService(user=request.user)
         out = service.generate_file()
 
         subject, from_email, to = "Documento para requisição de chaves", settings.EMAIL_HOST_USER, settings.ADMIN_EMAIL
         context = {
-            "requester": doc.creator.author_name,
-            "request_date": now.isoformat()
+            "requester": request.user.author_name,
+            "request_date": now.strftime("%Y-%m-%d, às %H:%M ") + settings.timezone
         }
         email_html_message = render_to_string(settings.KEYDOC_HTML, context)
         email_plaintext_message = render_to_string(settings.KEYDOC_TXT, context)
@@ -38,3 +36,14 @@ class KeyDocGenerate(APIView):
         msg.send()
 
         return Response(status=status.HTTP_200_OK)
+
+class KeyDocInfo(APIView):
+    def get(self, request):
+        try:
+            doc = KeyDoc.objects.latest('pk')
+        except KeyDoc.DoesNotExist:
+            return Response(status=status.HTTP_200_OK, data=json.dumps({"detail":"No document has been generated yet."}))
+
+        serialized = KeyDocSerializer(doc)
+        print(serialized.data)
+        return Response(serialized.data, status=status.HTTP_200_OK)
