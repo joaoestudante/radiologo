edd13e9b4b71cd6c16de77775be234046d9bc2c8
Ricardo Maçãs <universidade@ricardomacas.com>
2020-07-11 01:17:18 +0100

    Add FeedService task (preliminary)
    Added a task similar to the processing service to be scheduled using Celery

--- backend/src/programs/tasks.py
+++ backend/src/programs/tasks.py
@@ -2,7 +2,7 @@ from celery import shared_task
 
 from programs.models import Program
 from programs.services.processing.ProcessingService import ProcessingService
-
+from programs.services.rss_upload.FeedService import FeedService
 
 # TODO: Pass the PK and get the program, service gets simpler that way
 @shared_task
@@ -10,4 +10,11 @@ def process_audio(uploaded_file_path, program_pk, uploader, email, emission_date
     service = ProcessingService(path=uploaded_file_path, program_pk=program_pk, emission_date=emission_date,
                                 author_name=uploader, email=email)
     service.process()
+    return True
+
+# TODO: Schedule this task as recurring automatically for every program
+@shared_task
+def upload_from_feed(program_pk):
+    service = FeedService(program_pk)
+    service.download_last_episode() # Only runs if program.rss_feed_status = True
     return True
