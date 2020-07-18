f4a5d80e4c636d935349c0387856de4135f23144
João Lourenço <estudante.lourenco@gmail.com>
2020-07-18 15:33:36 +0100

    Add user info when returning token

--- backend/src/radiologo/jwtclaims.py
+++ backend/src/radiologo/jwtclaims.py
@@ -1,22 +1,19 @@
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from users.serializers.UserSerializer import UserSerializer
 
 
 class RadiologoTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
-
-        token['author_name'] = user.author_name
-
-        programs = {}
-        for p in user.program_set.all():
-            programs[p.pk] = p.name
-        token['programs'] = programs
-
         return token
 
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data["user"] = UserSerializer(self.user).data
+        return data
 
 class RadiologoTokenObtainPairView(TokenObtainPairView):
     permission_classes = [AllowAny]
